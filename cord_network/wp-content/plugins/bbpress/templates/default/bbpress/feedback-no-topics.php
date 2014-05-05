51f96ccdb4d16554ecf9aaedbadbe132142d1ce3
Thanasi <tomkay94@gmail.com>
2014-05-05 15:45:23 -0400

    Removed duplicate files, fixed user-forum login issue

---
+++ cord_network/wp-content/plugins/bbpress/templates/default/bbpress/feedback-no-topics.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * No Topics Feedback Part
+ *
+ * @package bbPress
+ * @subpackage Theme
+ */
+
+?>
+<!-- Gives error message when no topics are found on an empty forum
+<div class="bbp-template-notice">
+	<p><?php _e( 'Oh bother! No topics were found here!', 'bbpress' ); ?></p>
+</div>
+-->
---
+++ cord_network/wp-content/themes/cbox-mla-master/engine/includes/custom.php
@@ -0,0 +1,55 @@
+<?php
+
+// Custom avatars.
+require_once( 'avatars.php' );
+
+function modify_some_widgets(){
+
+   register_sidebar(array(
+      'name' => 'Homepage Center Left',
+      'id' => 'homepage-center-left',
+      'description' => "The Left Center Widget on the Homepage",
+      'before_widget' => '<div id="%1$s" class="widget %2$s">',
+      'after_widget' => '</div>',
+      'before_title' => '<h4>',
+      'after_title' => '</h4>'
+   ));
+
+   register_sidebar(array(
+      'name' => 'Homepage Center Middle',
+      'id' => 'homepage-center-middle',
+      'description' => "The Left Center Widget on the Homepage",
+      'before_widget' => '<div id="%1$s" class="widget %2$s">',
+      'after_widget' => '</div>',
+      'before_title' => '<h4>',
+      'after_title' => '</h4>'
+   ));
+
+   register_sidebar(array(
+      'name' => 'Homepage Center Right',
+      'id' => 'homepage-center-right',
+      'description' => "The Left Center Widget on the Homepage",
+      'before_widget' => '<div id="%1$s" class="widget %2$s">',
+      'after_widget' => '</div>',
+      'before_title' => '<h4>',
+      'after_title' => '</h4>'
+   ));
+
+   // Unregsiter CBox sidebar
+   unregister_sidebar( 'homepage-center-widget' );
+
+   // Publications
+   register_sidebar( array(
+      'id' => 'publications-sidebar',
+      'name' => 'Publications Sidebar',
+      'description' => 'The publications widget area',
+      'before_widget' => '<article id="%1$s" class="widget %2$s">',
+      'after_widget' => '</article>',
+      'before_title' => '<h4>',
+      'after_title' => '</h4>'
+   ));
+
+}
+add_action( 'widgets_init', 'modify_some_widgets', 9 );
+
+?>
